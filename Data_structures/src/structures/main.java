757554600c110fe052a82f2c64e64fe2ae444596
MK2345 <lapsus4@gmx.de>
2024-07-05 15:27:14 +0200

    added main class

---
+++ Data_structures/src/structures/main.java
@@ -0,0 +1,15 @@
+package structures;
+
+import java.util.LinkedList;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LinkedList<String> liste = new LinkedList<>();
+		
+
+	}
+
+}
